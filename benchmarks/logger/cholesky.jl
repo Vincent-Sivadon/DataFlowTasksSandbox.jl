4fa058c3017ff20b8505e16514851d255add51e8
Vincent-Sivadon <vincent.sivadon@laposte.net>
2022-06-17 11:58:16 +0200

    If ran on server

--- benchmarks/logger/cholesky.jl
+++ benchmarks/logger/cholesky.jl
@@ -3,6 +3,14 @@ using DataFlowTasks: R, W, RW
 using LinearAlgebra
 using DataFlowTasksSandbox
 
+# Desactivate plotting if it is a server
+servers = ["maury", "muscat", "maranges"]
+current = gethostname()
+is_server = isempty(findall(str -> str == current, a))
+if (is_server)
+    ENV["GKSwstype"]="nul"
+end
+
 DataFlowTasks.should_log() = true
 sch = DataFlowTasks.JuliaScheduler(500)
 DataFlowTasks.setscheduler!(sch)
@@ -13,11 +21,16 @@ work(A) = cholesky_dft!(A)
 
 # Context
 # -------
-DataFlowTasksSandbox.TILESIZE[] = 400
+nthreads = Threads.nthreads()
+tilesizes = 1000
+DataFlowTasksSandbox.TILESIZE[] = tilesizes
 n = 4000
 A = rand(n, n)
 A = (A + adjoint(A))/2
 A = A + n*I
 
+work(A)
 
 logging(work, TraceLog, A)
+
+# savefig("./fig/logger/cholesky_muscat_$(n)_$(tilesizes)_$nthreads.png")
--- src/cholesky/dft.jl
+++ src/cholesky/dft.jl
@@ -9,29 +9,25 @@ cholesky_dft!(A::Matrix, s=TILESIZE[]) = _cholesky_dft!(PseudoTiledMatrix(A, s))
 function _cholesky_dft!(A::PseudoTiledMatrix)
     m,n = size(A) # number of blocks
     for i in 1:m
-        # _chol!(A[i,i],UpperTriangular)
         @dspawn _chol!(A[i,i]) (A[i,i],) (RW,)
         Aii = A[i,i]
         U = UpperTriangular(Aii)
         L = adjoint(U)
         for j in i+1:n
             Aij = A[i,j]
-            # TriangularSolve.ldiv!(L,Aij,tturbo)
             @dspawn TriangularSolve.ldiv!(L,Aij, Val(false)) (Aii,Aij) (R,RW)
         end
         for j in i+1:m
             Aij = A[i,j]
             for k in j:n
-                # TODO: for k = j, only the upper part needs to be updated,
-                # dividing the cost of that operation by two
                 Ajk = A[j,k]
                 Aji = adjoint(Aij)
                 Aik = A[i,k]
-                # schur_complement!(Ajk,Aji,Aik,tturbo)
                 @dspawn Octavian.matmul_serial!(Ajk, Aji, Aik, -1, 1) (Ajk,Aij,Aik) (RW,R,R)
             end
         end
     end
+
     # wait for all computations before returning
     DataFlowTasks.sync()
     return Cholesky(A.data,'U',zero(BlasInt))
